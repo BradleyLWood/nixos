cf98133c08802a01a8cf903b95abfb3590fe5567
Bradley Wood <52896759+BradleyLWood@users.noreplay.github.com>
2026-07-14 12:46:23 -0600

    added tmux, starship config

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   imports =
@@ -74,7 +74,7 @@
 
     shellAliases = {
       update = "sudo nixos-rebuild switch -I nixos-config=$HOME/.nixconfig/configuration.nix";
-      config = "/run/current-system/sw/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME";
+      config = "${lib.getExe pkgs.git} --git-dir=$HOME/.dotfiles/ --work-tree=$HOME";
     };
 
     histSize = 10000;
@@ -84,14 +84,102 @@
     ];
 
     interactiveShellInit = ''
-      bindkey -v
       export KEYTIMEOUT=1
+      bindkey -v
+      bindkey '^L' 'autosuggest-accept'
     '';
+  };
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = true;
+      character = {
+        success_symbol = "[>](bold green)";
+        error_symbol = "[>](bold red)";
+        vimcmd_symbol = "[<](bold green)";
+        vimcmd_visual_symbol = "[<](bold yellow)";
+        vimcmd_replace_symbol = "[<](bold purple)";
+        vimcmd_replace_one_symbol = "[<](bold purple)";
+      };
+    };
   };
 
   programs.tmux = {
     enable = true;
+    escapeTime = 0;
     clock24 = false;
+    extraConfig = ''
+      set -g base-index 1
+      set -g pane-base-index 1
+      
+      set -g set-titles on
+      set -g set-titles-string '#{session_name} • #{window_index} • #{pane_title}'
+      set -g automatic-rename on
+      set -g automatic-rename-format '#{b:pane_current_path}'
+      
+      set -g default-terminal "screen-256color"
+      set -g default-shell ${pkgs.zsh}/bin/zsh
+      set -g default-command ${pkgs.zsh}/bin/zsh
+      set -ga terminal-overrides ",xterm-256color:Tc"
+
+      set -g detach-on-destroy off
+
+      set-window-option -g mode-keys vi
+      bind -T copy-mode-vi v send-keys -X begin-selection
+      bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel
+      bind p paste-buffer
+
+      unbind C-b
+      set -g prefix C-space
+      bind space send-prefix
+
+      set-window-option -g mode-keys vi
+      bind -T copy-mode-vi v send-keys -X begin-selection
+      bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel
+      bind p paste-buffer
+
+      bind r source-file ~/.config/tmux/tmux.conf \; display "Reloaded!"
+
+      bind -r h previous-window
+      bind -r j switch-client -n
+      bind -r k switch-client -p
+      bind -r l next-window
+      bind -r C-h select-pane -L
+      bind -r C-j select-pane -D
+      bind -r C-k select-pane -U
+      bind -r C-l select-pane -R
+      bind -r M-h resize-pane -L 5
+      bind -r M-j resize-pane -D 5
+      bind -r M-k resize-pane -U 5
+      bind -r M-l resize-pane -R 5
+
+      unbind s
+      unbind v
+      bind s split-window -h -c "#(pane_current_path)"
+      bind v split-window -v -c "#(pane_current_path)"
+
+      bind -r C-Left swap-window -t -1 \; select-window -t -1
+      bind -r C-Right swap-window -t +1 \; select-window -t +1
+
+      set -g status-position top
+
+      set -g status-left-length 100
+      set -g status-left "#[fg=blue,bg=#1e1e2e]  #[fg=#1e1e2e,bg=blue] #h │ #S #[fg=blue,bg=#1e1e2e]"
+      set -g status-left-style "bg=blue,fg=black"
+
+      set -g status-justify centre
+      set -g status-style "bg=default"
+      setw -g window-status-current-format '#[bold]#[fg=#cba6f7,bg=#1e1e2e]█#[fg=black,bg=#cba6f7]#I #W#[fg=#cba6f7,bg=#1e1e2e]█'
+      setw -g window-status-format ' #I #W '
+      setw -g window-status-separator ' '
+      setw -g window-status-style "bg=#1e1e2e"
+      setw -g window-status-current-style "bg=#cba6f7,fg=black"
+
+      set -g status-right-length 100
+      set -g status-right "#[fg=blue, bg=default] #[fg=black, bg=blue] %m/%d │ %I:%M %p #[fg=blue, bg=default]  "
+      set -g status-right-style "bg=default,fg=blue"
+    '';
   };
 
   programs.waybar = {
